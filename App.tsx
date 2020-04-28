fe11adc804d2681fb6cfbeedf025ac51d8650cd6
Strombilly <bp1250@messiah.edu>
2020-04-27 21:13:53 -0400

    Hello
    World

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! This is a test to see if word wrapping is enabled on the app by default.</Text>
+      <Text>Hello World.</Text>
     </View>
   );
 }
