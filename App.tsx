63250099b6a12ae8e4ecd07dc763600253cb9e52
Eric Weischedel <yshuttle@gmail.com>
2020-05-05 19:04:30 -0400

    Changing text

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World... do CircleCI tests please!</Text>
+      <Text>Hello World!</Text>
     </View>
   );
 }
