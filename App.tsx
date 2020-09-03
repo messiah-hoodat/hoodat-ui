17628bc1b26f671345deab17984f14c00be0b5bc
Eric Weischedel <eweischedel@scheduleengine.com>
2020-09-03 14:20:05 -0400

    Create screens and test folder

--- App.tsx
+++ App.tsx
@@ -2,23 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import SignInScreen from './SignInScreen';
-import SignUpScreen from './SignUpScreen';
-import { Text, } from 'react-native';
-
+import SignInScreen from './screens/SignInScreen';
+import SignUpScreen from './screens/SignUpScreen';
 
 const Stack = createStackNavigator();
-
-
 
 class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
         <Stack.Navigator headerMode='none'>
-          <Stack.Screen name="Sign In" component={SignInScreen} />
+          <Stack.Screen name="Sign In" component={SignInScreen}/>
           <Stack.Screen name="Sign Up" component={SignUpScreen}/>
-
         </Stack.Navigator>
       </NavigationContainer>
     );
