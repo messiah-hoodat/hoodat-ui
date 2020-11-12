e24dd76332bb947fef3cea4f7a112132309c07df
wesleycqy <wesleychong7@gmail.com>
2020-11-12 17:30:11 +0800

    Fixed Tab Navigation

--- App.tsx
+++ App.tsx
@@ -12,6 +12,10 @@ import AddForm from './screens/AddForm';
 import QuizScreen from './screens/QuizScreen';
 import QuizResultsScreen from './screens/QuizResultsScreen';
 import TestMultipleList from './screens/TestMultipleList';
+import sharedWithMe from './screens/sharedWithMe';
+import Groups from './screens/Groups';
+import Settings from './screens/Settings';
+import TestScreen from './screens/TestScreen';
 import { Entypo } from '@expo/vector-icons';
 import "react-native-gesture-handler";
 import {
@@ -25,68 +29,136 @@ import React, { useState } from "react";
 
 import { UserContext, UserState } from "./contexts/UserContext";
 
-const myListsStack = createStackNavigator();
-function HomeStackScreen() {
+
+
+
+const LoggedInTabNav = createBottomTabNavigator();
+function LoggedInTabNavScreen() {
   return (
-    <myListsStack.Navigator
-          headerMode="none"
-          screenOptions={{ gestureEnabled: false }}
-        >
-          <myListsStack.Screen name="Splash Screen" component={SplashScreen} />
-          <myListsStack.Screen name="Sign In" component={SignInScreen} />
-          <myListsStack.Screen name="My Lists" component={myListsScreen} />
-          <myListsStack.Screen name="Mult Lists Test" component={TestMultipleList} />
-          <myListsStack.Screen name="Add List" component={AddList}/>
-          <myListsStack.Screen name="Hoodat Buds" component={HoodatBudsList}/>
-          <myListsStack.Screen name="Quiz Screen" component={QuizScreen}/>
-          <myListsStack.Screen name="Quiz Results Screen" component={QuizResultsScreen}/>
-          <myListsStack.Screen name="Sign Up" component={SignUpScreen}/>
-          <myListsStack.Screen name="Add Form" component={AddForm}/>
-      </myListsStack.Navigator>
-   );
- }
-
- function SettingsStackScreen() {
-  return (
-    <View>
-      <Text style={{textAlign: 'center', marginTop: 300}}>Settings Screen</Text>
-    </View>
-  );
-}
-
-
-const Tab = createBottomTabNavigator();
-export default function App() {
-  const [value, setValue] = useState<UserState>({ token: "", userId: "" });
-
-  return (
-    <UserContext.Provider value={{ value, setValue }}>
-      <NavigationContainer>
-      <Tab.Navigator
+    <LoggedInTabNav.Navigator
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
           let iconName;
-          if (route.name === 'Home') {
+          if (route.name === 'My Lists') {
             iconName = focused
-            ? 'database'
-            : 'database';
-          } else if (route.name === 'Settings') {
+            ? 'home'
+            : 'home';
+          } 
+          else if (route.name === 'Shared With Me') {
             iconName = focused
             ? 'v-card'
             : 'v-card';
+          }
+          else if (route.name === 'Groups') {
+            iconName = focused
+            ? 'users'
+            : 'users';
+          }
+          else if (route.name === 'Settings') {
+            iconName = focused
+            ? 'dots-three-horizontal'
+            : 'dots-three-horizontal';
+          }
+          else {
+            iconName = focused
+            ? 'help'
+            : 'help';
           }
     
         return <Entypo name={iconName} size={size} color={color}     />;
           },
         })}
           tabBarOptions={{
-          activeTintColor: 'tomato',
+          activeTintColor: "#6EA8FF",
           inactiveTintColor: 'gray',
         }}>
 
-        <Tab.Screen name="Home" component={HomeStackScreen} />
-        <Tab.Screen name="Settings" component=     {SettingsStackScreen} />
-      </Tab.Navigator>
+        <LoggedInTabNav.Screen name="My Lists"     component=  {myListsScreen} />
+        <LoggedInTabNav.Screen name="Shared With Me" component=  {SharedWithMeStackScreen} />
+        <LoggedInTabNav.Screen name="Groups" component=  {GroupsStackScreen} />
+        <LoggedInTabNav.Screen name="Settings" component=  {SettingsStackScreen} />
+      </LoggedInTabNav.Navigator>
+
+  );
+}
+
+const myListsStack = createStackNavigator();
+function myListsStackScreen() {
+  return (
+    <myListsStack.Navigator
+          headerMode="none"
+          screenOptions={{ gestureEnabled: false }}
+        >
+          <myListsStack.Screen name="My Lists" component={LoggedInTabNavScreen} />
+          <myListsStack.Screen name="Mult Lists Test" component={TestMultipleList} />
+          <myListsStack.Screen name="Add List" component={AddList}/>
+          <myListsStack.Screen name="Hoodat Buds" component={HoodatBudsList}/>
+          <myListsStack.Screen name="Quiz Screen" component={QuizScreen}/>
+          <myListsStack.Screen name="Quiz Results Screen" component={QuizResultsScreen}/>
+          <myListsStack.Screen name="Add Form" component={AddForm}/>
+      </myListsStack.Navigator>
+   );
+ }
+
+const sharedWithMeStack = createStackNavigator();
+function SharedWithMeStackScreen() {
+  return (
+    <sharedWithMeStack.Navigator
+          headerMode="none"
+          screenOptions={{ gestureEnabled: false }}
+        >
+          <sharedWithMeStack.Screen name="Shared With Me" component={sharedWithMe} />
+          <sharedWithMeStack.Screen name="Test Screen" component={TestScreen}/>
+      </sharedWithMeStack.Navigator>
+  );
+}
+
+const groupsStack = createStackNavigator();
+function GroupsStackScreen() {
+  return (
+    <groupsStack.Navigator
+          headerMode="none"
+          screenOptions={{ gestureEnabled: false }}
+        >
+          <groupsStack.Screen name="Groups" component={Groups} />
+          <groupsStack.Screen name="Test Screen" component={TestScreen}/>
+      </groupsStack.Navigator>
+  );
+}
+
+const settingsStack = createStackNavigator();
+function SettingsStackScreen() {
+  return (
+    <settingsStack.Navigator
+          headerMode="none"
+          screenOptions={{ gestureEnabled: false }}
+        >
+          <settingsStack.Screen name="Settings" component={Settings} />
+          <settingsStack.Screen name="Test Screen" component={TestScreen}/>
+      </settingsStack.Navigator>
+  );
+}
+
+
+const fullAppStack = createStackNavigator();
+export default function App() {
+  const [value, setValue] = useState<UserState>({ token: "", userId: "" });
+
+  return (
+    <UserContext.Provider value={{ value, setValue }}>
+      <NavigationContainer>
+        <fullAppStack.Navigator
+            headerMode="none"
+            screenOptions={{ gestureEnabled: false }}
+          >
+            <fullAppStack.Screen name="Splash Screen" component={SplashScreen} />
+            <fullAppStack.Screen name="Sign In" component={SignInScreen} />
+            <fullAppStack.Screen name="Sign Up" component={SignUpScreen}/>
+            <fullAppStack.Screen name="My Lists" component={myListsStackScreen}/>
+            <fullAppStack.Screen name="Shared With Me" component={SharedWithMeStackScreen}/>
+            <fullAppStack.Screen name="Groups" component={GroupsStackScreen}/>
+            <fullAppStack.Screen name="Settings" component={SettingsStackScreen}/>
+        </fullAppStack.Navigator>
         
       </NavigationContainer>
     </UserContext.Provider>
---
+++ screens/Groups.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+} from "react-native";
+import Icon from "react-native-vector-icons/Entypo";
+
+
+
+
+class Groups extends React.Component{
+  
+
+  render() {
+    
+
+    return (
+      <View style={styles.container}>
+        <Text style = {{marginTop:200}}>Groups Screen</Text>
+        <Button title="Next" onPress={() => this.props.navigation.navigate("Test Screen")}></Button>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+  },
+
+  
+});
+
+export default Groups;
