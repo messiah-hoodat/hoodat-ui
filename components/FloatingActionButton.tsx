612d08b4a2700c9ae2091cf293119e6379b8a7cc
Eric Weischedel <yshuttle@gmail.com>
2020-11-17 17:23:21 -0500

    Add FAB

---
+++ components/FloatingActionButton.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+} from "react-native";
+import { FAB } from "react-native-paper";
+import { RFValue } from "react-native-responsive-fontsize";
+
+interface Props {
+  icon: string;
+  disabled?: boolean;
+  label?: string;
+  loading?: boolean;
+  onPress?: () => any;
+}
+
+export default function FloatingActionButton({ icon, disabled, label, loading, onPress }: Props)  {
+  return (
+    <View style={styles.container}>
+      <FAB
+        accessibilityValue={label}
+        color="white"
+        disabled={disabled}
+        focusable={true}
+        icon={icon}
+        label={label}
+        loading={loading}
+        onPress={onPress}
+        style={disabled ? styles.disabled : styles.fab}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    bottom: RFValue(20),
+    right: RFValue(20),
+  },
+
+  fab: {
+    backgroundColor: "#6EA8FF"
+  },
+
+  disabled: {
+    backgroundColor: "#B8D4FF"
+  }
+});
--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,3 @@
+export { default as FAB } from './FloatingActionButton';
 export { default as ListDetailsContactCard } from './ListDetailsContactCard';
 export { default as MultipleListsCard } from './MultipleListsCard';
